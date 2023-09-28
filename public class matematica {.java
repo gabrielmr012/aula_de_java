1a6e9b638c67be0efe6b3cbd891056d4b39212e4
Gabriel Marques Rosa <129209521+gabrielmr012@users.noreply.github.com>
2023-09-28 11:24:09 -0300

    Add files via upload

---
+++ public class matematica {.java
@@ -0,0 +1,23 @@
+public class matematica {
+//atributos
+    public double base, altura;
+//métodos
+    public void result(){
+        result = base * altura;
+        system.out.println("Resultado");
+
+//Métodos sao semelhantes as funções porém, nos métodos sao impressos os resultados
+
+    }
+
+//função
+
+public double cal(){
+    result = base * altura;
+    return result;
+//Função gera um valor para uma variavel atraves do calculo, mas nao o imprime, gera um valor para ser usado posteriormente por outra class
+//geralmente usamos para fazer um calculo e depois usar o msm 
+}
+
+
+}
